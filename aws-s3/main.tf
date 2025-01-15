4e7e7d3b64cfe90c1b71177a3df650071018dbfe
wafiq <wafiqgithub775@gmail.com>
2025-01-16 00:28:34 +0600

    add random_id provider & resource

--- aws-s3/main.tf
+++ aws-s3/main.tf
@@ -1,24 +1,32 @@
 terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.16"
+    required_providers {
+        aws = {
+            source  = "hashicorp/aws"
+            version = "~> 4.16"
+        }
+        random = {
+            source = "hashicorp/random"
+            version = "3.6.3"
+        }
     }
-  }
 
-  required_version = ">= 1.2.0"
+    required_version = ">= 1.2.0"
 }
 
 provider "aws" {
-  region  = var.region
+    region  = var.region
 }
 
-resource "aws_s3_bucket" "bucket" {
-  bucket = "tf-s3-bucket-123"
+resource "random_id" "rand_id" {
+    byte_length = 8
+}
+
+resource "aws_s3_bucket" "tf-bucket" {
+    bucket = "tf-s3-bucket-${random_id.rand_id.hex}"
 }
 
 resource "aws_s3_object" "bucket_data" {
-  bucket = aws_s3_bucket.bucket.bucket
-  key = "data.txt"
-  source = "./s3.txt"
+    bucket = aws_s3_bucket.tf-bucket.bucket
+    key = "data.txt"
+    source = "./s3.txt"
 }
--- aws-s3/output.tf
+++ aws-s3/output.tf
@@ -1,6 +1,6 @@
 output "aws_s3_bucket_name" {
-  value = aws_s3_bucket.bucket.bucket
+  value = aws_s3_bucket.tf-bucket.bucket
 }
 output "aws_s3_bucket_id" {
-  value = aws_s3_bucket.bucket.id
+  value = aws_s3_bucket.tf-bucket.id
 }
