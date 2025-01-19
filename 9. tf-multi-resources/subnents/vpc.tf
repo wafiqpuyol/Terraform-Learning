7517378f2caea30a78ec63d6c68a980de1004aea
wafiq <wafiqgithub775@gmail.com>
2025-01-20 05:40:30 +0600

    add 2 vpc using count

---
+++ 9. tf-multi-resources/subnents/vpc.tf
@@ -0,0 +1,19 @@
+locals {
+  project = "Project-1"
+}
+
+resource "aws_vpc" "tf_vpc" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "${local.project}-vpc"
+  }
+}
+
+resource "aws_subnet" "tf_subnet" {
+  vpc_id     = aws_vpc.tf_vpc.id
+  cidr_block = "10.0.${count.index}.0/24"
+  count      = 2
+  tags = {
+    Name = "${local.project}-subnet-${count.index}"
+  }
+}
